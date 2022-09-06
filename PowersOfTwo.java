9face4ec487b4c09c1da23f964b9b1ef993088e4
igor_pont <pontigor11@gmail.com>
2022-09-07 01:33:05 +0300

    fix PowersOfTwo.java

--- PowersOfTwo.java
+++ PowersOfTwo.java
@@ -3,7 +3,7 @@
  */
 public class PowersOfTwo {
     public static void main(String[] args) {
-        long[] array = powersOfTwo(5);
+        long[] array = powersOfTwo(3);
         for (long i : array) {
             System.out.printf("%d ", i);
         }
@@ -11,12 +11,15 @@ public class PowersOfTwo {
     }
 
     public static long[] powersOfTwo(int n) {
-        long[] exp = new long[n];
-        for (int i = 0; i < n; i++) {
-            exp[i] = ((int) Math.pow(2, i));
+        if (n == 0)
+            return new long[] { 1 };
+        else {
+            long[] exp = new long[n + 1];
+            for (int i = 0; i <= n; i++) {
+                exp[i] = ((int) Math.pow(2, i));
+            }
+            return exp;
         }
-        exp[n - 1] = (int) Math.pow(2, n);
-        return exp;
     }
 
 }
