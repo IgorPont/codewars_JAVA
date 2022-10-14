32230bf82c333cda9e40549226a3da9f790a0b9f
igor_pont <pontigor11@gmail.com>
2022-10-14 16:43:50 +0300

    add Kata

---
+++ Kata.java
@@ -0,0 +1,25 @@
+/*
+ * imple, given a string of words, return the length of the shortest word(s)
+ */
+
+public class Kata {
+
+    public static void main(String[] args) {
+        System.out.println(findShort("bitcoin take over the world maybe who knows perhaps"));
+
+    }
+
+    public static int findShort(String s) {
+        String[] words = s.split(" ");
+        int minLen = words[0].length();
+        String minWrd = words[0];
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() < minWrd.length()) {
+                minWrd = words[i];
+                minLen = words[i].length();
+
+            }
+        }
+        return minLen;
+    }
+}
